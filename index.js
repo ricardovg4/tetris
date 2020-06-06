68054fd96ed0db6dd48bc9a90089b89a38d99287
Ricardo <ricardovg4@hotmail.com>
2020-06-06 01:12:32 -0400

    gravity working

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // grid initialization
 const gameGrid = document.querySelector('.game-container__grid');
 let currentPosition = 4;
-const currentRotation = 0;
+let currentRotation = 0;
 const width = 10;
 const height = 20;
 let random = Math.floor(Math.random() * tetrominoes.length);
@@ -11,9 +11,12 @@ let currentIdentifier = tetrominoes[random];
 let current = currentIdentifier.rotation[currentRotation];
 
 //create 200 divs inside the game container
-for (let i = 0; i < 200; i++) {
+for (let i = 0; i < 210; i++) {
     const stdDiv = document.createElement('div');
     stdDiv.classList.add('game-container__square');
+    if (i >= 200) {
+        stdDiv.classList.add('set');
+    }
     gameGrid.appendChild(stdDiv);
 }
 
@@ -23,49 +26,91 @@ const squares = Array.from(
 );
 
 //variable that manages how to render the tetrominoes in the grid
-let tetrominoRender = {};
+let tetrominoRender = [];
+let tetrominoRenderIndex = [];
 
 //helper function to change the tetrominoRender if the tetromino is I which occupies an area of 16 instead of 9
 function renderHelper() {
     if (currentIdentifier.tetromino === 'i') {
-        tetrominoRender = {
-            0: squares[currentPosition],
-            1: squares[currentPosition + 1],
-            2: squares[currentPosition + 2],
-            3: squares[currentPosition + 3],
-            4: squares[currentPosition + width],
-            5: squares[currentPosition + width + 1],
-            6: squares[currentPosition + width + 2],
-            7: squares[currentPosition + width + 3],
-            8: squares[currentPosition + width * 2],
-            9: squares[currentPosition + width * 2 + 1],
-            10: squares[currentPosition + width * 2 + 2],
-            11: squares[currentPosition + width * 2 + 3],
-            12: squares[currentPosition + width * 3],
-            13: squares[currentPosition + width * 3 + 1],
-            14: squares[currentPosition + width * 3 + 2],
-            15: squares[currentPosition + width * 3 + 3]
-        };
+        tetrominoRenderIndex = [
+            currentPosition,
+            currentPosition + 1,
+            currentPosition + 2,
+            currentPosition + 3,
+            currentPosition + width,
+            currentPosition + width + 1,
+            currentPosition + width + 2,
+            currentPosition + width + 3,
+            currentPosition + width * 2,
+            currentPosition + width * 2 + 1,
+            currentPosition + width * 2 + 2,
+            currentPosition + width * 2 + 3,
+            currentPosition + width * 3,
+            currentPosition + width * 3 + 1,
+            currentPosition + width * 3 + 2,
+            currentPosition + width * 3 + 3
+        ];
+        tetrominoRender = [
+            squares[tetrominoRenderIndex[0]],
+            squares[tetrominoRenderIndex[1]],
+            squares[tetrominoRenderIndex[2]],
+            squares[tetrominoRenderIndex[3]],
+            squares[tetrominoRenderIndex[4]],
+            squares[tetrominoRenderIndex[5]],
+            squares[tetrominoRenderIndex[6]],
+            squares[tetrominoRenderIndex[7]],
+            squares[tetrominoRenderIndex[8]],
+            squares[tetrominoRenderIndex[9]],
+            squares[tetrominoRenderIndex[10]],
+            squares[tetrominoRenderIndex[11]],
+            squares[tetrominoRenderIndex[12]],
+            squares[tetrominoRenderIndex[13]],
+            squares[tetrominoRenderIndex[14]],
+            squares[tetrominoRenderIndex[15]]
+        ];
     } else {
-        tetrominoRender = {
-            0: squares[currentPosition],
-            1: squares[currentPosition + 1],
-            2: squares[currentPosition + 2],
-            3: squares[currentPosition + width],
-            4: squares[currentPosition + width + 1],
-            5: squares[currentPosition + width + 2],
-            6: squares[currentPosition + width * 2],
-            7: squares[currentPosition + width * 2 + 1],
-            8: squares[currentPosition + width * 2 + 2]
-        };
+        tetrominoRenderIndex = [
+            currentPosition,
+            currentPosition + 1,
+            currentPosition + 2,
+            currentPosition + width,
+            currentPosition + width + 1,
+            currentPosition + width + 2,
+            currentPosition + width * 2,
+            currentPosition + width * 2 + 1,
+            currentPosition + width * 2 + 2
+        ];
+        tetrominoRender = [
+            squares[tetrominoRenderIndex[0]],
+            squares[tetrominoRenderIndex[1]],
+            squares[tetrominoRenderIndex[2]],
+            squares[tetrominoRenderIndex[3]],
+            squares[tetrominoRenderIndex[4]],
+            squares[tetrominoRenderIndex[5]],
+            squares[tetrominoRenderIndex[6]],
+            squares[tetrominoRenderIndex[7]],
+            squares[tetrominoRenderIndex[8]]
+        ];
     }
 }
 
+function dropTetromino() {
+    random = Math.floor(Math.random() * tetrominoes.length);
+    currentPosition = 4;
+    currentRotation = 0;
+}
+
+let tetrominoDraw;
+let tetrominoDrawIndex;
 function draw() {
     renderHelper();
+    tetrominoDraw = [];
+    tetrominoDrawIndex = [];
     for (let tetro in tetrominoRender) {
         if (current[tetro] === 1) {
             tetrominoRender[tetro].classList.add('painted');
+            tetrominoDraw.push(tetrominoRender[tetro]);
+            tetrominoDrawIndex.push(tetrominoRenderIndex[tetro]);
         }
     }
 }
@@ -83,8 +128,20 @@ function gravity() {
     undraw();
     currentPosition += width;
     draw();
+    boundary();
 }
 
-draw();
+function boundary() {
+    if (
+        tetrominoDrawIndex.some((tetro) =>
+            squares[tetro + width].classList.contains('set')
+        )
+    ) {
+        tetrominoDraw.forEach((tetro) => {
+            tetro.classList.add('set');
+            dropTetromino();
+        });
+    }
+}
 
-setInterval(gravity, 1000);
+setInterval(gravity, 100);
